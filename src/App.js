daffe63d98f530a09bc5cb4109dbbcea094080ca
Tevaryan Thiagarajan <tevaryan1993@gmail.com>
2019-11-26 18:04:34 +0800

    changed email name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
+import { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: '',
+      lname: '',
+      email: '',
+      message: '',
+      mailSent: false,
+      error: null
+    }
+  }
+
+
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    axios({
+      method: 'post',
+      url: 'http://localhost:8080/api/index.php',
+      headers: { 'content-type': 'application/json' },
+      withCredentials: false,
+      data: this.state
+    })
+      .then(result => {
+        this.setState({
+          mailSent: result.data.sent
+        })
+
+      })
+      .catch(error => this.setState({ error: error.message }));
+
+  }
+
+
+  render () {
+    return (
+      <div className="App">
+    <p>Contact Me</p>
+    <div>
+    <form action="#" >
+      <label>First Name</label>
+      <input type="text" id="fname" name="firstname" placeholder="Your name.."
+        value={this.state.fname}
+        onChange={e => this.setState({ fname: e.target.value })}
+      />
+      <label>Last Name</label>
+      <input type=" text" id="lname" name="lastname" placeholder="Your last name.."
+        value={this.state.lname}
+        onChange={e => this.setState({ lname: e.target.value })}
+      />
+
+
+      <label>Email</label>
+      <input type="email" id="email" name="email" placeholder="Your email"
+        value={this.state.email}
+        onChange={e => this.setState({ email: e.target.value })}
+      />
+
+
+      <label>Message</label>
+      <textarea id="message" name="message" placeholder="Write something.."
+        onChange={e => this.setState({ message: e.target.value })}
+        value={this.state.message}
+      ></textarea>
+      <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
+    </form >
+    </div>
+    <div>
+        {this.state.mailSent &&
+          <div>Thank you for contcting us.</div>
+        }
+      </div>
+  </div>
+    )
+
+  }
+}
+  export default App;
